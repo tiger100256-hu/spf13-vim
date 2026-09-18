9e73c0c045ce0e2bb2b8a759d7c7b77e7ef120f7
Hu Yuan <yuan2.hu@intel.com>
2026-09-18 16:13:08 +0800

    fix install script
    Signed-off-by: Hu Yuan <yuan2.hu@intel.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -124,14 +124,15 @@ create_symlinks() {
     lnif "$source_path/.vimrc"         "$target_path/.vimrc"
     lnif "$source_path/.vimrc.bundles" "$target_path/.vimrc.bundles"
     lnif "$source_path/.vimrc.before"  "$target_path/.vimrc.before"
+    lnif "$source_path/.vimrc.local"   "$target_path/.vimrc.local"
+    lnif "$source_path/.vimrc.bundles.local" "$target_path/.vimrc.bundles.local"
+    lnif "$source_path/.vimrc.before.local"  "$target_path/.vimrc.before.local"
     lnif "$source_path/.vim"           "$target_path/.vim"
 
     if program_exists "nvim"; then
         lnif "$source_path/.vim"       "$target_path/.config/nvim"
         lnif "$source_path/.vimrc"     "$target_path/.config/nvim/init.vim"
     fi
-
-    touch  "$target_path/.vimrc.local"
 
     ret="$?"
     success "Setting up vim symlinks."
